e3f0025482361dcc6471b0ff3389b09eb0e29963
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-03-01 00:35:43 +0530

    Password Generator

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -3,19 +3,19 @@ import "./Check.css";
 const Checkbox = () => {
   return (
     <div className="check">
-      <div className="upperCase">
+      <div className="Case">
         <input type="checkbox" />
         <label>Include uppercase letters</label>
       </div>
-      <div className="upperCase">
+      <div className="Case">
         <input type="checkbox" />
         <label>Include lowercase letters</label>
       </div>
-      <div className="upperCase">
+      <div className="Case">
         <input type="checkbox" />
         <label>Include numbers</label>
       </div>
-      <div className="upperCase">
+      <div className="Case">
         <input type="checkbox" />
         <label>Include symbols</label>
       </div>
