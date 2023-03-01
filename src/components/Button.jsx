78a31c14c867f3aee6fa9c6d398c731173da8dbd
Abhishek97Ojha <abhishek97ojha@gmail.com>
2023-03-01 15:00:16 +0530

    Password Generator

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./Button.css";
-const Button = ({ genPass,onClick}) => {
+const Button = ({
+  upperCase,
+  lowerCase,
+  numbers,
+  symbols,
+  length,
+  genPass,
+  onClick,
+}) => {
   return (
     <div className="button">
-      <button onClick={()=>{
-        onClick()
-      }}>{genPass?"Password Generated":"Generate Password"}</button>
+      <button
+        onClick={() => {
+          onClick();
+        }}
+      >
+        {genPass && length > 0 && (upperCase || lowerCase || numbers || symbols)
+          ? "Password Generated"
+          : "Generate Password"}
+      </button>
     </div>
   );
 };
--- src/components/PasswordGenerator.jsx
+++ src/components/PasswordGenerator.jsx
@@ -3,9 +3,23 @@ import "./Password.css";
 import Checkbox from "./Checkbox";
 import Length from "./Length";
 import Button from "./Button";
-import { numberArray, upperCaseArray, lowerCaseArray, symbolsArray } from "./Constants";
+import {
+  numberArray,
+  upperCaseArray,
+  lowerCaseArray,
+  symbolsArray,
+} from "./Constants";
 import { MdFileCopy } from "react-icons/md";
-const PasswordGenerator = ({ pass, setPass, input, setInput ,copy,setCopy,genPass,setGenPass}) => {
+const PasswordGenerator = ({
+  pass,
+  setPass,
+  input,
+  setInput,
+  copy,
+  setCopy,
+  genPass,
+  setGenPass,
+}) => {
   const lengthChange = (value) => {
     setPass({
       ...pass,
@@ -38,54 +52,62 @@ const PasswordGenerator = ({ pass, setPass, input, setInput ,copy,setCopy,genPas
   };
   const generatePass = () => {
     // console.log(input,pass);
-    const {upperCase,lowerCase,numbers,symbols,length} = pass;
-    const generateWord = (upperCase,lowerCase,numbers,symbols)=>{
-        const availValue = [
-            ...(upperCase? upperCaseArray : []),
-            ...(lowerCase? lowerCaseArray : []),
-            ...(numbers? numberArray : []),
-            ...(symbols? symbolsArray : []),
-        ];
-        const shuffleArray =(array) => {
-            return array.sort (()=>{
-                return Math.random() - 0.5
-            })
-        }
-        const result = shuffleArray(availValue).slice(0,length).join("")
-        setInput(result)
-        setGenPass(true)
-        setTimeout(()=>{
-            setGenPass(false);
-        },1000)
-    }
-    generateWord(upperCase,lowerCase,numbers,symbols)
+    const { upperCase, lowerCase, numbers, symbols, length } = pass;
+    const generateWord = (upperCase, lowerCase, numbers, symbols) => {
+      const availValue = [
+        ...(upperCase ? upperCaseArray : []),
+        ...(lowerCase ? lowerCaseArray : []),
+        ...(numbers ? numberArray : []),
+        ...(symbols ? symbolsArray : []),
+      ];
+      const shuffleArray = (array) => {
+        return array.sort(() => {
+          return Math.random() - 0.5;
+        });
+      };
+      const result = shuffleArray(availValue).slice(0, length).join("");
+      setInput(result);
+      setGenPass(true);
+      setTimeout(() => {
+        setGenPass(false);
+      }, 1000);
+    };
+    generateWord(upperCase, lowerCase, numbers, symbols);
   };
 
   return (
     <div className="container">
       <h1>Password Generator</h1>
       <div className="inputBox">
-        <input
-          type="text"
-          value={input}
-         
-        />
-        <button onClick={()=>{
-            if(input.length>0){
-                navigator.clipboard.writeText(input)
-                setCopy(true)
-                setTimeout(()=>{
-                    setCopy(false);
-                },1000)
+        <input type="text" value={input} />
+        <button
+          onClick={() => {
+            if (input.length > 0) {
+              navigator.clipboard.writeText(input);
+              setCopy(true);
+              setTimeout(() => {
+                setCopy(false);
+              }, 1000);
             }
-        }}>{copy?"Copied":<MdFileCopy />}</button>
+          }}
+        >
+          {copy ? "Copied" : <MdFileCopy />}
+        </button>
       </div>
       <Length onChange={lengthChange} />
       <Checkbox label="Include uppercase letters" onChange={upperChange} />
       <Checkbox label="Include lowercase letters" onChange={lowerChange} />
       <Checkbox label="Include numbers" onChange={numChange} />
       <Checkbox label="Include symbols" onChange={symbolChange} />
-      <Button onClick={generatePass} genPass={genPass}/>
+      <Button
+        onClick={generatePass}
+        genPass={genPass}
+        length={pass.length}
+        upperCase={pass.upperCase}
+        lowerCase={pass.lowerCase}
+        numbers={pass.numbers}
+        symbols={pass.symbols}
+      />
     </div>
   );
 };
